3e7b0415465d464d1cfb68a00cf2663d906ed0ec
Oliver Vea <oliver.vea@gmail.com>
2024-10-26 17:23:46 +0900

    Adding nuget test project

---
+++ NugetTests/SimpleCsvTests.cs
@@ -0,0 +1,26 @@
+/*
+using CsvData;
+
+namespace Tests;
+
+public class SimpleCsvTests
+{
+    private static readonly object[][] TestData =
+    [
+        [Simple.Fireball, 15, 5, 15],
+        [Simple.Frostbolt, 12, 5, 20],
+        [Simple.HeavyStrike, 20, 0.7f, 5]
+    ];
+
+    [TestCaseSource(nameof(TestData))]
+    public void VerifySpellValues(Simple.Spell spell, int expectedDamage, float expectedRange, int expectedCost)
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(spell.Damage, Is.EqualTo(expectedDamage));
+            Assert.That(spell.Range, Is.EqualTo(expectedRange));
+            Assert.That(spell.Cost, Is.EqualTo(expectedCost));
+        });
+    }
+}
+*/
---
+++ README.md
@@ -0,0 +1,13 @@
+# Static CSV Source Generator
+
+This package allows for generating static C# classes allowing for easy, type safe and performant row-wise access to the data.
+
+## Usage
+
+Add a package reference to your project:
+
+```xml
+    <ItemGroup>
+        <PackageReference Include="CsvSourceGeneration" Version="0.0.1"/>
+    </ItemGroup>
+```
