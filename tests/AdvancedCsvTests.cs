716420f0085a76063a57a9386d081d4292ca721c
Oliver Vea <oliver.vea@gmail.com>
2024-10-26 16:39:24 +0900

    Adding workflow

--- tests/AdvancedCsvTests.cs
+++ tests/AdvancedCsvTests.cs
@@ -4,14 +4,14 @@ namespace Tests;
 
 public class AdvancedCsvTests
 {
-    private static readonly object[][] Spells =
+    private static readonly object[][] TestData =
     [
         [Advanced.Melee, "melee", TimeSpan.FromSeconds(1), 5.0f, 1.0f, 0f, 0f],
         [Advanced.Ranged, "ranged", TimeSpan.FromSeconds(1.5), 5.0f, 10.0f, 0f, 0f],
         [Advanced.Fireball, "fireball", TimeSpan.FromSeconds(3), 100.0f, 10.0f, 3.0f, 5.0f]
     ];
 
-    [TestCaseSource(nameof(Spells))]
+    [TestCaseSource(nameof(TestData))]
     public void VerifyAbilityValues(Advanced.Ability ability, string expectedIdString, TimeSpan expectedCooldown,
         float expectedDamage, float expectedRange, float expectedAoERange, float expectedAoEDamage)
     {
--- tests/SimpleCsvTests.cs
+++ tests/SimpleCsvTests.cs
@@ -4,18 +4,21 @@ namespace Tests;
 
 public class SimpleCsvTests
 {
-    private static readonly object[][] Spells =
+    private static readonly object[][] TestData =
     [
         [Simple.Fireball, 15, 5, 15],
         [Simple.Frostbolt, 12, 5, 20],
         [Simple.HeavyStrike, 20, 0.7f, 5]
     ];
 
-    [TestCaseSource(nameof(Spells))]
+    [TestCaseSource(nameof(TestData))]
     public void VerifySpellValues(Simple.Spell spell, int expectedDamage, float expectedRange, int expectedCost)
     {
-        Assert.That(spell.Damage, Is.EqualTo(expectedDamage));
-        Assert.That(spell.Range, Is.EqualTo(expectedRange));
-        Assert.That(spell.Cost, Is.EqualTo(expectedCost));
+        Assert.Multiple(() =>
+        {
+            Assert.That(spell.Damage, Is.EqualTo(expectedDamage));
+            Assert.That(spell.Range, Is.EqualTo(expectedRange));
+            Assert.That(spell.Cost, Is.EqualTo(expectedCost));
+        });
     }
 }
